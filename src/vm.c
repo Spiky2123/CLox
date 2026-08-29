09682c486ac8798bd8c64c2a2b06a5f248a36e8a
Spiky2123 <scutarumatei@gmail.com>
2026-08-29 17:19:02 +0300

    CLox chapter 19 challenges - Optimized string allocation

--- src/vm.c
+++ src/vm.c
@@ -59,7 +59,8 @@ static void concatenate() {
     memcpy(chars + a->length, b->chars, b->length);
     chars[length] = '\0';
 
-    ObjString* result = takeString(chars, length);
+    ObjString* result = copyString(chars, length);
+    FREE_ARRAY(char, chars, length + 1);
     push(OBJ_VAL(result));
 }
 
