90f57a9afb7d65ba33c428dd8953b20b89db868b
Spiky2123 <scutarumatei@gmail.com>
2026-09-04 22:52:26 +0300

    CLox chapter 25 - Closures

--- src/object.c
+++ src/object.c
@@ -40,16 +40,30 @@ static uint32_t hashString(const char *key, int length) {
     return hash;
 }
 
-ObjFunction * newFunction() {
-    ObjFunction* function = ALLOCATE_OBJ(ObjFunction, OBJ_FUNCTION);
+ObjClosure *newClosure(ObjFunction *function) {
+    ObjUpvalue **upvalues = ALLOCATE(ObjUpvalue*, function->upvalueCount);
+    for (int i = 0; i < function->upvalueCount; i++) {
+        upvalues[i] = NULL;
+    }
+
+    ObjClosure *closure = ALLOCATE_OBJ(ObjClosure, OBJ_CLOSURE);
+    closure->function = function;
+    closure->upvalues = upvalues;
+    closure->upvalueCount = function->upvalueCount;
+    return closure;
+}
+
+ObjFunction *newFunction() {
+    ObjFunction *function = ALLOCATE_OBJ(ObjFunction, OBJ_FUNCTION);
     function->arity = 0;
+    function->upvalueCount = 0;
     function->name = NULL;
     initChunk(&function->chunk);
     return function;
 }
 
-ObjNative * newNative(NativeFn function) {
-    ObjNative* native = ALLOCATE_OBJ(ObjNative, OBJ_NATIVE);
+ObjNative *newNative(NativeFn function) {
+    ObjNative *native = ALLOCATE_OBJ(ObjNative, OBJ_NATIVE);
     native->function = function;
     return native;
 }
@@ -71,7 +85,15 @@ ObjString *copyString(const char *chars, int length) {
     return allocateString(heapChars, length, hash);
 }
 
-static void printFunction(ObjFunction* function) {
+ObjUpvalue *newUpvalue(Value *slot) {
+    ObjUpvalue *upvalue = ALLOCATE_OBJ(ObjUpvalue, OBJ_UPVALUE);
+    upvalue->closed = NIL_VAL;
+    upvalue->location = slot;
+    upvalue->next = NULL;
+    return upvalue;
+}
+
+static void printFunction(ObjFunction *function) {
     if (function->name == NULL) {
         printf("<script>");
         return;
@@ -81,6 +103,10 @@ static void printFunction(ObjFunction* function) {
 
 void printObject(Value value) {
     switch (OBJ_TYPE(value)) {
+        case OBJ_CLOSURE: {
+            printFunction(AS_CLOSURE(value)->function);
+            break;
+        }
         case OBJ_FUNCTION: {
             printFunction(AS_FUNCTION(value));
             break;
@@ -89,8 +115,13 @@ void printObject(Value value) {
             printf("<native fn>");
             break;
         }
-        case OBJ_STRING:
+        case OBJ_STRING: {
             printf("%s", AS_CSTRING(value));
             break;
+        }
+        case OBJ_UPVALUE: {
+            printf("upvalue");
+            break;
+        }
     }
 }
